6d6894c232e10b5cf1b25d660df882f007872b56
eneassena <desenvolvedoreneas2000@gmail.com>
2022-08-27 02:14:11 -0300

    add react-bootstrap no frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
-import './ui/style/App.css';
+// import './ui/style/App.css';
 import { list } from './data/service/ApiService.js';
 import axios from 'axios';
+import { Nav, Table, Row, Col } from 'react-bootstrap'
+
 
 const Listagem = () => {
     const [data, setData] = useState(list)
@@ -23,44 +25,83 @@ const Listagem = () => {
             console.log(err);
         })
     }
+
+    function showClients() {
+        axios.get('http://localhost:8000/cliente-api/clientes')
+        .then(function(data){
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
     useEffect(() => {
         getTodos();
+        showClients()
     }, []);
 
     return (
-        <>
-            <table>
-                <thead>
-                    <tr>
-                        <th>#Id</th>
-                        <th>Title</th>
-                        <th>Body</th>
-                    </tr>
-                </thead>
-                <tbody>
-                { data.map(item => (
-                    <tr key={item.id}>
-                        <td>{item.id}</td>
-                        <td>{item.title}</td>
-                        <td>{item.body}</td>
-                    </tr>
-                )) }
+        <> 
+        <Row>
 
-                </tbody>
-            </table>
+            <Col>
+                <Table striped bordered hover>
+                <thead>
+                        <tr>
+                            <th>#Id</th>
+                            <th>Title</th>
+                            <th>Body</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    { data.map(item => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.title}</td>
+                            <td>{item.body}</td>
+                        </tr>
+                    )) }
+
+                    </tbody>
+                </Table>
+            </Col>
+        </Row>
+             
         </>
     )
 }
 
-
+const HeaderApp = () => {
+    return (
+        <nav>
+            <Nav
+            activeKey="/home"
+            onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+            >
+            <Nav.Item>
+                <Nav.Link href="/home">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-1">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-2">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="disabled" disabled>
+                Disabled
+                </Nav.Link>
+            </Nav.Item>
+            </Nav>
+        </nav>
+    )
+}
 
 function App() {
   return (
     <div>
-     <h1>Olá</h1>
-
-     <Listagem />
-
+        <HeaderApp />
+        <Listagem />
     </div>
   );
 }
